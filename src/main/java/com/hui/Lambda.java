a5c375b8806018cf298931d4e4472c4c259190ad
eric <942300755@qq.com>
2024-01-15 01:02:12 +0800

    commit

---
+++ src/main/java/com/hui/Lambda.java
@@ -0,0 +1,17 @@
+package com.hui;
+
+/**
+ * @author Eirk
+ * @Description
+ * @Date 2024/1/15 0:56
+ */
+
+interface MyInterface {
+    int sum(int i, int j);
+}
+
+public class Lambda {
+    public static void main(String[] args) {
+
+    }
+}
