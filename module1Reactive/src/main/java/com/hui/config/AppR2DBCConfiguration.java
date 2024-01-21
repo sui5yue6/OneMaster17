938af809305ca4a7041a64b835ad11e094599339
eric <942300755@qq.com>
2024-01-21 23:54:49 +0800

    commit

---
+++ module1Reactive/src/main/java/com/hui/config/AppR2DBCConfiguration.java
@@ -0,0 +1,16 @@
+package com.hui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+/**
+ * @author Eirk
+ * @Description
+ * @Date 2024/1/21 23:54
+ */
+@Configuration
+@EnableR2dbcRepositories
+public class AppR2DBCConfiguration {
+
+
+}
